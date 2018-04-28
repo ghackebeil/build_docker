c365680f79012b2965e96af7d03195bce6752809
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2018-04-28 16:08:47 -0400

    fixing issue with adding gams directory to path

--- docker_scripts/install_gams.sh
+++ docker_scripts/install_gams.sh
@@ -9,14 +9,14 @@ RUN echo "" && \
 ARG PREFIX="/root"
 ARG TARGET="linux_x64_64_sfx.exe"
 ARG VERSION="25.0.3"
+ARG GAMSDIR="gams25.0_linux_x64_64_sfx"
 RUN mkdir ${PREFIX}/GAMS_${VERSION}
 RUN cd ${PREFIX}/GAMS_${VERSION} && \
     wget -q "https://d37drm4t2jghv5.cloudfront.net/distributions/${VERSION}/linux/${TARGET}"
 RUN cd ${PREFIX}/GAMS_${VERSION} && chmod u+x ${TARGET}
 RUN cd ${PREFIX}/GAMS_${VERSION} && ./${TARGET} > /dev/null
 RUN cd ${PREFIX}/GAMS_${VERSION} && rm ${TARGET}
-ARG GAMSDIR="`ls ${PREFIX}/GAMS_${VERSION}`"
-ENV PATH="${GAMSDIR}:${PATH}"
+ENV PATH="${PREFIX}/GAMS_${VERSION}/${GAMSDIR}:${PATH}"
 # TODO: Install GAMS Python API
 # python 2.6
 #cd ${GAMSDIR}/apifiles/Python/api_26 && python setup.py install
