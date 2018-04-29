de4b1ee829d27aed62e7cab08b6528439718dd91
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2018-04-28 21:29:27 -0400

    add git and subversion

--- install_scripts/install_libs.sh
+++ install_scripts/install_libs.sh
@@ -11,6 +11,7 @@ RUN apt-get -q update && \
     apt-get -q -y --no-install-recommends install \
         build-essential libssl-dev libffi-dev \
         wget zip unzip \
+        git subversion \
         gfortran \
         libopenblas-dev \
         openmpi-bin openmpi-common libopenmpi-dev \
