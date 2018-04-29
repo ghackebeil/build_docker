b79e697d710ae54c4603197158ea78f5a4b11ade
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2018-04-29 11:10:13 -0400

    fix

--- install_scripts/install_baron.sh
+++ install_scripts/install_baron.sh
@@ -14,17 +14,18 @@ RUN cd ${PREFIX} && \
     rm -rf ${TARGET}.zip
 ENV PATH="${PREFIX}/${TARGET}:${PATH}"
 ARG TARGET
+# dummy baron input file
 RUN echo 'OPTIONS {\n\
-          results: 1;\n\
-          ResName: \"dummy.res\";\n\
-          summary: 1;\n\
-          SumName: \"dummy.sum\";\n\
-          times: 1;\n\
-          TimName: \"dummy.tim\";\n\
-          }\n\
-          POSITIVE_VARIABLES x1;\n\
-          OBJ: minimize x1;\n' > dummy.bar
+results: 1;\n\
+ResName: \"dummy.res\";\n\
+summary: 1;\n\
+SumName: \"dummy.sum\";\n\
+times: 1;\n\
+TimName: \"dummy.tim\";\n\
+}\n\
+POSITIVE_VARIABLES x1;\n\
+OBJ: minimize x1;\n' > dummy.bar
 # note the one can not assign the output of a command to an 'ENV var=' declaration
-# so we must use "RUN export var='
+# so we must use 'RUN export var='
 RUN export BARON_VERSION=`baron -f dummy.bar | grep -Po 'BARON version .*\. Built' | grep -Po '\d.\d.\d'`
 RUN rm -f dummy.bar dummy.res dummy.sum dummy.tim dummy.prob
