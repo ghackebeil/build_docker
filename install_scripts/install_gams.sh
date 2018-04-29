7e06bacd386986d3314b9836b4b1d61ed11a118f
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2018-04-28 20:35:41 -0400

    install gams python api

--- install_scripts/install_gams.sh
+++ install_scripts/install_gams.sh
@@ -17,12 +17,26 @@ RUN mkdir ${PREFIX}/GAMS_${VERSION} && \
     ./${TARGET} > /dev/null && \
     rm ${TARGET}
 ENV PATH="${PREFIX}/GAMS_${VERSION}/${GAMSDIR}:${PATH}"
-# TODO: Install GAMS Python API
+# Install GAMS Python API
 # python 2.6
-#cd ${GAMSDIR}/apifiles/Python/api_26 && python setup.py install
+RUN python -c "import __pypy__" 2> /dev/null || \
+    [[ "$(python -c'import sys;print(sys.version_info[:2])')" != "(2, 6)" ]] || \
+    (cd ${PREFIX}/GAMS_${VERSION}/${GAMSDIR}/apifiles/Python/api_26 && python setup.py install)
 # python 2.7
-#cd ${GAMSDIR}/apifiles/Python/api && python setup.py install
-# python 3.4 / 3.5
-#cd ${GAMSDIR}/apifiles/Python/api_34 && python setup.py install
+RUN python -c "import __pypy__" 2> /dev/null || \
+    [[ "$(python -c'import sys;print(sys.version_info[:2])')" != "(2, 7)" ]] || \
+    (cd ${PREFIX}/GAMS_${VERSION}/${GAMSDIR}/apifiles/Python/api && python setup.py install)
+# python 3.4
+RUN python -c "import __pypy__" 2> /dev/null || \
+    [[ "$(python -c'import sys;print(sys.version_info[:2])')" != "(3, 4)" ]] || \
+    (cd ${PREFIX}/GAMS_${VERSION}/${GAMSDIR}/apifiles/Python/api_34 && python setup.py install)
+# python 3.5 (uses same install directory as 3.4)
+RUN python -c "import __pypy__" 2> /dev/null || \
+    [[ "$(python -c'import sys;print(sys.version_info[:2])')" != "(3, 4)" ]] || \
+    (cd GAMS_25.0.3/gams25.0_linux_x64_64_sfx/apifiles/Python/api_34 && python setup.py install)
 # python 3.6
-#cd ${GAMSDIR}/apifiles/Python/api_36 && python setup.py install
+RUN python -c "import __pypy__" 2> /dev/null || \
+    [[ "$(python -c'import sys;print(sys.version_info[:2])')" != "(3, 6)" ]] || \
+    (cd ${PREFIX}/GAMS_${VERSION}/${GAMSDIR}/apifiles/Python/api_36 && python setup.py install)
+# make sure the install worked
+RUN python -c "import gams"
