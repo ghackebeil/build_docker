6313b70e7b7a67884bba6c10b724ff5d15666bf7
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2018-04-28 21:35:46 -0400

    no gams python api for 3.5

--- install_scripts/install_gams.sh
+++ install_scripts/install_gams.sh
@@ -31,26 +31,28 @@ RUN mkdir ${PREFIX}/GAMS_${VERSION} && \
     rm -r ${PREFIX}/GAMS_${VERSION}/${GAMSDIR}/apifiles/GAMS && \
     rm -r ${PREFIX}/GAMS_${VERSION}/${GAMSDIR}/apifiles/VBnet
 ENV PATH="${PREFIX}/GAMS_${VERSION}/${GAMSDIR}:${PATH}"
-# Install GAMS Python API (but not on PyPy)
+# Install GAMS Python API (but not on PyPy or CPython_3.5)
 # python 2.6
 RUN python -c "import __pypy__" 2> /dev/null || \
     [ "$(python -c'import sys;print(sys.version_info[:2])')" != "(2, 6)" ] || \
-    (cd ${PREFIX}/GAMS_${VERSION}/${GAMSDIR}/apifiles/Python/api_26 && python setup.py install)
+    (cd ${PREFIX}/GAMS_${VERSION}/${GAMSDIR}/apifiles/Python/api_26 && \
+     python setup.py install && \
+     python -c "import gams")
 # python 2.7
 RUN python -c "import __pypy__" 2> /dev/null || \
     [ "$(python -c'import sys;print(sys.version_info[:2])')" != "(2, 7)" ] || \
-    (cd ${PREFIX}/GAMS_${VERSION}/${GAMSDIR}/apifiles/Python/api && python setup.py install)
+    (cd ${PREFIX}/GAMS_${VERSION}/${GAMSDIR}/apifiles/Python/api && \
+     python setup.py install && \
+     python -c "import gams")
 # python 3.4
 RUN python -c "import __pypy__" 2> /dev/null || \
     [ "$(python -c'import sys;print(sys.version_info[:2])')" != "(3, 4)" ] || \
-    (cd ${PREFIX}/GAMS_${VERSION}/${GAMSDIR}/apifiles/Python/api_34 && python setup.py install)
-# python 3.5 (uses same install directory as 3.4)
-RUN python -c "import __pypy__" 2> /dev/null || \
-    [ "$(python -c'import sys;print(sys.version_info[:2])')" != "(3, 5)" ] || \
-    (cd ${PREFIX}/GAMS_${VERSION}/${GAMSDIR}/apifiles/Python/api_34 && python setup.py install)
+    (cd ${PREFIX}/GAMS_${VERSION}/${GAMSDIR}/apifiles/Python/api_34 && \
+     python setup.py install && \
+     python -c "import gams")
 # python 3.6
 RUN python -c "import __pypy__" 2> /dev/null || \
     [ "$(python -c'import sys;print(sys.version_info[:2])')" != "(3, 6)" ] || \
-    (cd ${PREFIX}/GAMS_${VERSION}/${GAMSDIR}/apifiles/Python/api_36 && python setup.py install)
-# make sure the install worked
-RUN python -c "import __pypy__" 2> /dev/null || python -c "import gams"
+    (cd ${PREFIX}/GAMS_${VERSION}/${GAMSDIR}/apifiles/Python/api_36 && \
+     python setup.py install && \
+     python -c "import gams")
