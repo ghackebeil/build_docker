d84cf884b2e2fa03bb7834a193bc6b71bbd28e87
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2018-04-30 17:21:16 -0400

    ci

--- install_scripts/install_libs.sh
+++ install_scripts/install_libs.sh
@@ -7,7 +7,6 @@ RUN echo "" && \
     echo "======================" && \
     echo ""
 RUN apt-get -q update && \
-    apt-get -q upgrade -y && \
     apt-get -q -y --no-install-recommends install \
         build-essential libssl-dev libffi-dev \
         wget zip unzip \
