2d8049226ab07b9fc79bd3960766494d86b6c8e0
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2018-04-30 13:00:00 -0400

    ci

--- install_scripts/install_baron.sh
+++ install_scripts/install_baron.sh
@@ -11,11 +11,8 @@ RUN cd ${PREFIX} && \
     rm -rf ${TARGET}.zip && \
     wget -q "https://www.minlp.com/downloads/xecs/baron/current/${TARGET}.zip" && \
     unzip -q ${TARGET}.zip && \
-    rm -rf ${TARGET}.zip
-ENV PATH="${PREFIX}/${TARGET}:${PATH}"
-ARG TARGET
-# dummy baron input file
-RUN echo 'OPTIONS {\n\
+    rm -rf ${TARGET}.zip && \
+    echo 'OPTIONS {\n\
 results: 1;\n\
 ResName: "dummy.res";\n\
 summary: 1;\n\
@@ -24,9 +21,8 @@ times: 1;\n\
 TimName: "dummy.tim";\n\
 }\n\
 POSITIVE_VARIABLES x1;\n\
-OBJ: minimize x1;\n' > dummy.bar
-# note the one can not assign the output of a command to an 'ENV var=' declaration
-# so we must use 'RUN export var='
-RUN echo BARON_VERSION=`baron -f dummy.bar | grep -Po 'BARON version .*\. Built' | grep -Po '\d+.\d+.\d+'`
-RUN echo BARON_VERSION=`baron -f dummy.bar | grep -Po 'BARON version .*\. Built' | grep -Po '\d+.\d+.\d+'` >> ${DYNAMIC_VARS_FILE}
-RUN rm -f dummy.bar dummy.res dummy.sum dummy.tim dummy.prob
+OBJ: minimize x1;\n' > dummy.bar && \
+    echo BARON_VERSION=`baron -f dummy.bar | grep -Po 'BARON version .*\. Built' | grep -Po '\d+.\d+.\d+'` >> ${DYNAMIC_VARS_FILE} && \
+    rm -f dummy.bar dummy.res dummy.sum dummy.tim dummy.prob
+ENV PATH="${PREFIX}/${TARGET}:${PATH}"
+ARG TARGET
