cb913f7227d59b1cc82a4ab8f41769cd0f796c8f
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2018-04-30 12:40:31 -0400

    ci

--- install_scripts/install_baron.sh
+++ install_scripts/install_baron.sh
@@ -27,5 +27,9 @@ POSITIVE_VARIABLES x1;\n\
 OBJ: minimize x1;\n' > dummy.bar
 # note the one can not assign the output of a command to an 'ENV var=' declaration
 # so we must use 'RUN export var='
+RUN baron -f dummy.bar
+RUN baron -f dummy.bar | grep -Po 'BARON version .*\. Built'
+RUN baron -f dummy.bar | grep -Po 'BARON version .*\. Built' | grep -Po '\d.\d.\d'
+RUN echo BARON_VERSION=`baron -f dummy.bar | grep -Po 'BARON version .*\. Built' | grep -Po '\d.\d.\d'`
 RUN echo BARON_VERSION=`baron -f dummy.bar | grep -Po 'BARON version .*\. Built' | grep -Po '\d.\d.\d'` >> ${DYNAMIC_VARS_FILE}
 RUN rm -f dummy.bar dummy.res dummy.sum dummy.tim dummy.prob
