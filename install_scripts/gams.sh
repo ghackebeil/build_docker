b94860545489d04c1652afa03f97c02a7510666c
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2018-11-26 16:17:38 -0500

    updating gams version

--- install_scripts/gams.sh
+++ install_scripts/gams.sh
@@ -5,7 +5,7 @@ RUN echo "" && \
     echo ""
 ARG TARGET="linux_x64_64_sfx.exe"
 ARG GAMS_DIR="gams25.1_linux_x64_64_sfx"
-ENV GAMS_VERSION="25.1.2"
+ENV GAMS_VERSION="25.1.3"
 ENV PATH="${PREFIX}/GAMS_${GAMS_VERSION}/${GAMS_DIR}:${PATH}"
 RUN mkdir ${PREFIX}/GAMS_${GAMS_VERSION} && \
     cd ${PREFIX}/GAMS_${GAMS_VERSION} && \
