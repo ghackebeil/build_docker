a8c764323f083c67b800011b18c9c764c5dbb9cf
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2019-02-05 21:23:26 -0500

    working around bug with 19.0

--- install_scripts/python_libs.sh
+++ install_scripts/python_libs.sh
@@ -3,12 +3,12 @@ RUN echo "" && \
     echo "INSTALLING PYTHON LIBS" && \
     echo "======================" && \
     echo ""
-RUN pip install --no-cache-dir -U \
+RUN python -O -m pip install --no-cache-dir -U \
       pip \
       setuptools \
       wheel
-RUN (python -c "import __pypy__" &> /dev/null && pip install --no-cache-dir numpy==1.15.4) || pip install --no-cache-dir numpy
-RUN pip install --no-cache-dir \
+RUN (python -c "import __pypy__" &> /dev/null && python -O -m pip install --no-cache-dir numpy==1.15.4) || python -O -m pip install --no-cache-dir numpy
+RUN python -O -m pip install --no-cache-dir \
       sphinx \
       sphinx_rtd_theme \
       virtualenv \
@@ -24,12 +24,12 @@ RUN pip install --no-cache-dir \
       pymysql \
       xlrd
 # These may fail on PyPy
-RUN pip install --no-cache-dir scipy || echo failed to install scipy
-RUN pip install --no-cache-dir matplotlib || echo failed to install matplotlib
-RUN pip install --no-cache-dir seaborn || echo failed to install seaborn
-RUN pip install --no-cache-dir PyYAML || \
-    pip install https://github.com/yaml/pyyaml/archive/4.1.zip || \
+RUN python -O -m pip install --no-cache-dir scipy || echo failed to install scipy
+RUN python -O -m pip install --no-cache-dir matplotlib || echo failed to install matplotlib
+RUN python -O -m pip install --no-cache-dir seaborn || echo failed to install seaborn
+RUN python -O -m pip install --no-cache-dir PyYAML || \
+    python -O -m pip install https://github.com/yaml/pyyaml/archive/4.1.zip || \
     echo failed to install PyYAML
-RUN pip install --no-cache-dir numba || echo failed to install numba
-RUN pip install --no-cache-dir pandas || echo failed to install pandas
-RUN pip install --no-cache-dir pyodbc || echo failed to install pyodbc
+RUN python -O -m pip install --no-cache-dir numba || echo failed to install numba
+RUN python -O -m pip install --no-cache-dir pandas || echo failed to install pandas
+RUN python -O -m pip install --no-cache-dir pyodbc || echo failed to install pyodbc
