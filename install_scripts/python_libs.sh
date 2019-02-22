1b62147c15c3c9421028256140a48def51b9a6aa
John Siirola <jsiirola@users.noreply.github.com>
2019-02-22 14:56:28 -0700

    Defining the DOCKER_PYTHON_SLIM environment variable

--- install_scripts/python_libs.sh
+++ install_scripts/python_libs.sh
@@ -7,10 +7,12 @@ RUN pip install -U \
       pip \
       setuptools \
       wheel
-RUN pip install \
-      sphinx \
+
+ENV DOCKER_PYTHON_CORE=virtualenv
+RUN pip install ${DOCKER_PYTHON_CORE}
+
+ENV DOCKER_PYTHON_EXTRAS=sphinx \
       sphinx_rtd_theme \
-      virtualenv \
       cffi \
       numpy \
       mpi4py \
@@ -24,8 +26,15 @@ RUN pip install \
       xlrd \
       z3-solver \
       pint
+RUN pip install ${DOCKER_PYTHON_EXTRAS}
+
 # These currently fail on PyPy
-RUN (python -c "import __pypy__" 2> /dev/null) || (pip install scipy matplotlib pandas seaborn)
+ENV DOCKER_PYTHON_NOT_PYPY=scipy \
+    matplotlib \
+    pandas \
+    seaborn
+RUN (python -c "import __pypy__" 2> /dev/null) || (pip install ${DOCKER_PYTHON_NOT_PYPY})
+
 # These may not work on PyPy / Python3.7
 RUN pip install PyYAML || \
     pip install https://github.com/yaml/pyyaml/archive/4.1.zip || \
@@ -33,3 +42,11 @@ RUN pip install PyYAML || \
 RUN pip install numba || echo failed to install numba
 RUN pip install pyodbc || echo failed to install pyodbc
 RUN pip list
+
+# These are the Python packages that should be removed to return to a
+# "SLIM" build
+ENV DOCKER_PYTHON_SLIM=${DOCKER_PYTHON_EXTRAS} \
+    ${DOCKER_PYTHON_NOT_PYPY} \
+    pyyaml \
+    numba \
+    pyodbc
