a03995aa6e74c516a92b99c207a65b21181ef9ab
John Siirola <jsiirola@users.noreply.github.com>
2019-02-22 15:09:37 -0700

    Removing quotes that were not necessary; adding test/coverage packages

--- install_scripts/python_libs.sh
+++ install_scripts/python_libs.sh
@@ -8,10 +8,10 @@ RUN pip install -U \
       setuptools \
       wheel
 
-ENV DOCKER_PYTHON_CORE=virtualenv
+ENV DOCKER_PYTHON_CORE=virtualenv nose coverage codecov
 RUN pip install ${DOCKER_PYTHON_CORE}
 
-ENV DOCKER_PYTHON_EXTRAS="sphinx \
+ENV DOCKER_PYTHON_OPTIONAL=sphinx \
       sphinx_rtd_theme \
       cffi \
       numpy \
@@ -25,19 +25,17 @@ ENV DOCKER_PYTHON_EXTRAS="sphinx \
       pymysql \
       xlrd \
       z3-solver \
-      pint \
-"
-RUN pip install ${DOCKER_PYTHON_EXTRAS}
+      pint
+RUN pip install ${DOCKER_PYTHON_OPTIONAL}
 
 # These currently fail on PyPy
-ENV DOCKER_PYTHON_NOT_PYPY="scipy \
+ENV DOCKER_PYTHON_NOT_PYPY=scipy \
     matplotlib \
     pandas \
-    seaborn \
-"
+    seaborn
 RUN (python -c "import __pypy__" 2> /dev/null) || (pip install ${DOCKER_PYTHON_NOT_PYPY})
 
-# These may not work on PyPy / Python3.7
+# These are fragile and may not work on PyPy / Python3.7
 RUN pip install PyYAML || \
     pip install https://github.com/yaml/pyyaml/archive/4.1.zip || \
     echo failed to install PyYAML
@@ -47,9 +45,8 @@ RUN pip list
 
 # These are the Python packages that should be removed to return to a
 # "SLIM" build
-ENV DOCKER_PYTHON_SLIM="${DOCKER_PYTHON_EXTRAS} \
+ENV DOCKER_PYTHON_SLIM=${DOCKER_PYTHON_OPTIONAL} \
     ${DOCKER_PYTHON_NOT_PYPY} \
     pyyaml \
     numba \
-    pyodbc \
-"
+    pyodbc
